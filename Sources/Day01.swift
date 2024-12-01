94a4ba490c45aabe0cbc70794a3ac4442b346676
Mikhail Sivykh <m.sivykh@cian.ru>
2024-12-01 11:48:58 +0300

    Last preparation

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -1,20 +1,20 @@
 import Algorithms
 
-struct Day00: AdventDay {
+struct Day01: AdventDay {
     // Save your data in a corresponding text file in the `Data` directory.
     var data: String
     
     // Splits input data into its component parts and convert from string.
     var entities: [[Int]] {
-        data.split(separator: "\n\n").map {
-            $0.split(separator: "\n").compactMap { Int($0) }
+        data.split(separator: "\n").map {
+            $0.split(separator: "   ").compactMap { Int($0) }
         }
     }
     
     // Replace this with your solution for the first part of the day's challenge.
     func part1() -> Any {
-        // Calculate the sum of the first set of input data
-        entities.first?.reduce(0, +) ?? 0
+        
+        print(entities)
     }
     
     // Replace this with your solution for the second part of the day's challenge.
---
+++ Tests/Day01.swift
@@ -0,0 +1,27 @@
+import XCTest
+
+@testable import AdventOfCode
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+final class Day01Tests: XCTestCase {
+  // Smoke test data provided in the challenge question
+  let testData = """
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
+    """
+
+  func testPart1() throws {
+    let challenge = Day00(data: testData)
+    XCTAssertEqual(String(describing: challenge.part1()), "6000")
+  }
+
+  func testPart2() throws {
+    let challenge = Day00(data: testData)
+    XCTAssertEqual(String(describing: challenge.part2()), "32000")
+  }
+}
