4b98a3d6b54af177ecf56f0e17f1fce82e25dd9a
Mikhail Sivykh <m.sivykh@cian.ru>
2024-12-02 22:49:41 +0300

    added automation to start new day

--- Sources/AdventOfCode.swift
+++ Sources/AdventOfCode.swift
@@ -2,9 +2,9 @@ import ArgumentParser
 
 // Add each new day implementation to this array:
 let allChallenges: [any AdventDay] = [
-    Day00(),
+	Day02(),
     Day01(),
-    Day02()
+    Day00()
 ]
 
 @main
