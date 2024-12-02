783c8a5e6a75e6e6fe6cd1328327ed77326146e2
Mikhail Sivykh <m.sivykh@cian.ru>
2024-12-02 08:57:38 +0300

    Day 2

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -11,33 +11,44 @@ struct Day02: AdventDay {
     }
 
     func part1() -> Any {
-        let input = entities
-        var res = 0
-        for row in input {
-            if !(1...3 ~= abs(row[0] - row[1])) {
-                continue
-            }
-            let d = row[0] > row[1]
-            res += 1
-            for j in 2..<row.count where d != (row[j - 1] > row[j]) || !(1...3 ~= abs(row[j - 1] - row[j])) {
-                res -= 1
-                break
-            }
-        }
-
-        return res
+        entities.reduce(0, { $0 + (badIndices(row: $1).isEmpty ? 1 : 0) })
     }
 
     func part2() -> Any {
         let input = entities
         var res = 0
         for row in input {
-            var good = 1
-            if good >= 0 {
+            let indices = badIndices(row: row)
+            if indices.isEmpty {
                 res += 1
+                continue
+            }
+            for j in indices {
+                var row = row
+                row.remove(at: j)
+                if badIndices(row: row).isEmpty {
+                    res += 1
+                    break
+                }
             }
         }
 
         return res
+    }
+
+    private func badIndices(row: [Int]) -> [Int] {
+        guard 1...3 ~= abs(row[0] - row[1]) else {
+            return [0, 1]
+        }
+
+        let order = row[0] > row[1]
+        guard order == (row[1] > row[2]) else {
+            return [0, 1, 2]
+        }
+
+        for j in 2..<row.count where order != (row[j - 1] > row[j]) || !(1...3 ~= abs(row[j - 1] - row[j])) {
+            return [j - 1, j]
+        }
+        return []
     }
 }
