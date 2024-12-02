f3ea84ac621ff8c5b5a53e033ad83f48891e37b6
Mikhail Sivykh <m.sivykh@cian.ru>
2024-12-02 08:34:02 +0300

    Day 2, part 1

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -6,21 +6,38 @@ struct Day02: AdventDay {
 
     var entities: [[Int]] {
         data.split(separator: "\n").map {
-            $0.split(separator: "   ").compactMap { Int($0) }
+            $0.split(separator: " ").compactMap { Int($0) }
         }
     }
 
     func part1() -> Any {
         let input = entities
+        var res = 0
+        for row in input {
+            if !(1...3 ~= abs(row[0] - row[1])) {
+                continue
+            }
+            let d = row[0] > row[1]
+            res += 1
+            for j in 2..<row.count where d != (row[j - 1] > row[j]) || !(1...3 ~= abs(row[j - 1] - row[j])) {
+                res -= 1
+                break
+            }
+        }
 
-
-        return 0
+        return res
     }
 
     func part2() -> Any {
         let input = entities
+        var res = 0
+        for row in input {
+            var good = 1
+            if good >= 0 {
+                res += 1
+            }
+        }
 
-
-        return 0
+        return res
     }
 }
--- Tests/Day02.swift
+++ Tests/Day02.swift
@@ -7,15 +7,21 @@ import XCTest
 final class Day02Tests: XCTestCase {
     // Smoke test data provided in the challenge question
     let testData = """
+    7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
     """
 
     func testPart1() throws {
         let challenge = Day02(data: testData)
-        XCTAssertEqual(String(describing: challenge.part1()), "0")
+        XCTAssertEqual(String(describing: challenge.part1()), "2")
     }
 
     func testPart2() throws {
         let challenge = Day02(data: testData)
-        XCTAssertEqual(String(describing: challenge.part2()), "0")
+        XCTAssertEqual(String(describing: challenge.part2()), "4")
     }
 }
