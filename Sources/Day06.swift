b1c99f2c3164344e93bfc29e798093453b970eb0
Mikhail Sivykh <m.sivykh@cian.ru>
2024-12-06 09:35:36 +0300

    Day6, part 2

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -48,52 +48,44 @@ struct Day06: AdventDay {
         let n = grid[0].count
 
         var obstructions = [[Bool]](repeating: [Bool](repeating: false, count: n), count: m)
-        var oLines = [[Int]](repeating: [], count: m)
-        var oCols = [[Int]](repeating: [], count: n)
-        var current = (row: 0, col: 0)
+        var start = (row: 0, col: 0)
         for i in 0..<m {
             for j in 0..<n {
                 obstructions[i][j] = grid[i][j] == "#"
                 if grid[i][j] == "^" {
-                    current = (i, j)
-                }
-                if obstructions[i][j] {
-                    oLines[i].append(j)
-                    oCols[j].append(i)
+                    start = (i, j)
                 }
             }
         }
-
         var res = 0
         let moves = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-        var moveIndex = 0
-        while 0..<m ~= current.row, 0..<n ~= current.col {
-            var next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
-            while 0..<m ~= next.row && 0..<n ~= next.col && obstructions[next.row][next.col] {
-                moveIndex = (moveIndex + 1) % moves.count
-                next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
-            }
-            var cycle = true
-            if current != next {
 
+        for i in 0..<m {
+            for j in 0..<n {
+                guard !obstructions[i][j] else {
+                    continue
+                }
+                var current = start
+                var visited = [[[Int]]](repeating: [[Int]](repeating: [], count: n), count: m)
+                obstructions[i][j] = true
+                var moveIndex = 0
+                while 0..<m ~= current.row, 0..<n ~= current.col {
+                    if visited[current.row][current.col].contains(moveIndex) {
+                        res += 1
+                        break
+                    }
+                    visited[current.row][current.col].append(moveIndex)
+                    var next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
+                    while 0..<m ~= next.row && 0..<n ~= next.col && obstructions[next.row][next.col] {
+                        moveIndex = (moveIndex + 1) % moves.count
+                        next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
+                    }
+                    current = next
+                }
+                obstructions[i][j] = false
             }
-            current = next
         }
         return res
     }
 
-    func bsearch(x: Int, in array: [Int]) -> Int {
-        var l = 0, r = array.count
-        if x < array[0] { return 0 }
-        if x > array.last! { return array.count }
-        while l <= r {
-            let m = (l + r) / 2
-            if array[m] < x {
-                l = m + 1
-            } else {
-                r = m - 1
-            }
-        }
-        return l
-    }
 }
