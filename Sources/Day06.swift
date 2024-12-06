9df1d8538a762652799c4b2d184d64be9b78d751
Sivykh Mikhail <m.sivykh@cian.ru>
2024-12-06 11:05:10 +0300

    Refactored Day6 to the final version

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -4,46 +4,11 @@ import Collections
 struct Day06: AdventDay {
     var data: String
     
-    var entities: [[Character]] {
-        data.split(separator: "\n").map {
+    var entities: (m: Int, n: Int, (row: Int, col: Int), [[Bool]]) {
+        let grid = data.split(separator: "\n").map {
             Array($0)
         }
-    }
-    
-    func part1() -> Any {
-        let grid = entities
-        let m = grid.count
-        let n = grid[0].count
-
-        var obstructions = [[Bool]](repeating: [Bool](repeating: false, count: n), count: m)
-        var visited = [[Bool]](repeating: [Bool](repeating: false, count: n), count: m)
-        var current = (row: 0, col: 0)
-        for i in 0..<m {
-            for j in 0..<n {
-                obstructions[i][j] = grid[i][j] == "#"
-                if grid[i][j] == "^" {
-                    current = (i, j)
-                }
-            }
-        }
-        var res = 0
-        let moves = [[-1, 0], [0, 1], [1, 0], [0, -1]]
-        var moveIndex = 0
-        while 0..<m ~= current.row, 0..<n ~= current.col {
-            res += visited[current.row][current.col] ? 0 : 1
-            visited[current.row][current.col] = true
-            var next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
-            while 0..<m ~= next.row && 0..<n ~= next.col && obstructions[next.row][next.col] {
-                moveIndex = (moveIndex + 1) % moves.count
-                next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
-            }
-            current = next
-        }
-        return res
-    }
-    
-    func part2() -> Any {
-        let grid = entities
+        
         let m = grid.count
         let n = grid[0].count
 
@@ -57,6 +22,17 @@ struct Day06: AdventDay {
                 }
             }
         }
+        
+        return (m, n, start, obstructions)
+    }
+    
+    func part1() -> Any {
+        let (m, n, start, obstructions) = entities
+        return calc(m: m, n: n, start: start, obstructions: obstructions).distinct
+    }
+    
+    func part2() -> Any {
+        var (m, n, start, obstructions) = entities
         var res = 0
         let moves = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
@@ -65,27 +41,33 @@ struct Day06: AdventDay {
                 guard !obstructions[i][j] else {
                     continue
                 }
-                var current = start
-                var visited = [[[Int]]](repeating: [[Int]](repeating: [], count: n), count: m)
                 obstructions[i][j] = true
-                var moveIndex = 0
-                while 0..<m ~= current.row, 0..<n ~= current.col {
-                    if visited[current.row][current.col].contains(moveIndex) {
-                        res += 1
-                        break
-                    }
-                    visited[current.row][current.col].append(moveIndex)
-                    var next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
-                    while 0..<m ~= next.row && 0..<n ~= next.col && obstructions[next.row][next.col] {
-                        moveIndex = (moveIndex + 1) % moves.count
-                        next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
-                    }
-                    current = next
-                }
+                res += calc(m: m, n: n, start: start, obstructions: obstructions).cycle ? 1 : 0
                 obstructions[i][j] = false
             }
         }
         return res
     }
-
+    
+    private func calc(m: Int, n: Int, start: (row: Int, col: Int), obstructions: [[Bool]]) -> (distinct: Int, cycle: Bool) {
+        var current = start
+        var distinct = 0
+        var visited = [[[Bool]]](repeating: [[Bool]](repeating: [false, false, false, false], count: n), count: m)
+        let moves = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+        var moveIndex = 0
+        while 0..<m ~= current.row, 0..<n ~= current.col {
+            if visited[current.row][current.col][moveIndex] {
+                return (distinct, true)
+            }
+            distinct += visited[current.row][current.col].contains(true) ? 0 : 1
+            visited[current.row][current.col][moveIndex] = true
+            var next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
+            while 0..<m ~= next.row && 0..<n ~= next.col && obstructions[next.row][next.col] {
+                moveIndex = (moveIndex + 1) % moves.count
+                next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
+            }
+            current = next
+        }
+        return (distinct, false)
+    }
 }
