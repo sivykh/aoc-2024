720b692af4ff2821b68f860b81792d4e68d24831
Sivykh Mikhail <m.sivykh@cian.ru>
2024-12-06 11:27:02 +0300

    Removed unused code

--- Sources/Day06.swift
+++ Sources/Day06.swift
@@ -4,7 +4,7 @@ import Collections
 struct Day06: AdventDay {
     var data: String
     
-    var entities: (m: Int, n: Int, (row: Int, col: Int), [[Bool]]) {
+    var entities: (m: Int, n: Int, (row: Int, col: Int), obstructions: [Bool]) {
         let grid = data.split(separator: "\n").map {
             Array($0)
         }
@@ -12,14 +12,12 @@ struct Day06: AdventDay {
         let m = grid.count
         let n = grid[0].count
 
-        var obstructions = [[Bool]](repeating: [Bool](repeating: false, count: n), count: m)
+        var obstructions = [Bool](repeating: false, count: m * n)
         var start = (row: 0, col: 0)
-        for i in 0..<m {
-            for j in 0..<n {
-                obstructions[i][j] = grid[i][j] == "#"
-                if grid[i][j] == "^" {
-                    start = (i, j)
-                }
+        for ij in product(0..<m, 0..<n) {
+            obstructions[ij.0 * n + ij.1] = grid[ij.0][ij.1] == "#"
+            if grid[ij.0][ij.1] == "^" {
+                start = ij
             }
         }
         
@@ -34,20 +32,18 @@ struct Day06: AdventDay {
     func part2() -> Any {
         var (m, n, start, obstructions) = entities
         var res = 0
-        for i in 0..<m {
-            for j in 0..<n {
-                guard !obstructions[i][j] else {
-                    continue
-                }
-                obstructions[i][j] = true
-                res += calc(m: m, n: n, start: start, obstructions: obstructions).cycle ? 1 : 0
-                obstructions[i][j] = false
+        for ij in product(0..<m, 0..<n) {
+            guard !obstructions[ij.0 * n + ij.1] else {
+                continue
             }
+            obstructions[ij.0 * n + ij.1] = true
+            res += calc(m: m, n: n, start: start, obstructions: obstructions).cycle ? 1 : 0
+            obstructions[ij.0 * n + ij.1] = false
         }
         return res
     }
     
-    private func calc(m: Int, n: Int, start: (row: Int, col: Int), obstructions: [[Bool]]) -> (distinct: Int, cycle: Bool) {
+    private func calc(m: Int, n: Int, start: (row: Int, col: Int), obstructions: [Bool]) -> (distinct: Int, cycle: Bool) {
         let moves = [[-1, 0], [0, 1], [1, 0], [0, -1]]
         var current = start
         var distinct = 0
@@ -62,7 +58,7 @@ struct Day06: AdventDay {
             distinct += visited[visitedIndex0..<(visitedIndex0 + moves.count)].contains(true) ? 0 : 1
             visited[visitedIndex] = true
             var next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
-            while 0..<m ~= next.row && 0..<n ~= next.col && obstructions[next.row][next.col] {
+            while 0..<m ~= next.row && 0..<n ~= next.col && obstructions[next.row * n + next.col] {
                 moveIndex = (moveIndex + 1) % moves.count
                 next = (row: current.row + moves[moveIndex][0], col: current.col + moves[moveIndex][1])
             }
