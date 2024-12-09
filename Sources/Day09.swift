a78c952eda184b497b8a4d33b7610bc2d5d95648
Mikhail Sivykh <m.sivykh@cian.ru>
2024-12-09 14:07:48 +0300

    Day 9, part 2

--- Sources/Day09.swift
+++ Sources/Day09.swift
@@ -3,28 +3,25 @@ import Collections
 
 struct Day09: AdventDay {
     var data: String
-    // 2333133121414131402
-    // 00...111...2...333.44.5555.6666.777.888899
-    // 0099811188827773336446555566..............
 
     var entities: [Int] {
         data.utf8CString.dropLast().map({ Int($0 - 48) })
     }
     
+    // 2333133121414131402
+    // 00...111...2...333.44.5555.6666.777.888899
+    // 0099811188827773336446555566..............
     func part1() -> Any {
-        let e = entities
-        var input = e.count % 2 == 0 ? e.dropLast() : e
+        var input = entities
         var lastFileID = input.count / 2
         var lastFileIndex = input.count - 1
 
         var res = 0, len = 0
-        for i in 0..<input.count {
+        for i in 0..<input.count where input[i] > 0 {
             let thisLen = input[i]
             if i % 2 == 0 {
-                for j in 0..<input[i] {
-                    res += (len + j) * (i / 2)
-                    input[i] -= 1
-                }
+                res += (0..<input[i]).reduce(0, { $0 + (len + $1) * (i / 2) })
+                input[i] = 0
             } else {
                 for j in 0..<input[i] {
                     guard input[lastFileIndex] > 0 else {
@@ -43,9 +40,40 @@ struct Day09: AdventDay {
         return res
     }
     
+    // 2333133121414131402
+    // 00...111...2...333.44.5555.6666.777.888899
+    // 00992111777.44.333....5555.6666.....8888..
     func part2() -> Any {
-        let e = entities
-        
-        return 0
+        let input = entities
+        var copy = input
+        var res = 0, len = 0
+
+        for i in 0..<copy.count {
+            let thisLen = input[i]
+            if i % 2 == 0 && copy[i] > 0 {
+                res += (0..<copy[i]).reduce(0, { $0 + (len + $1) * (i / 2) })
+                copy[i] = 0
+            } else {
+                var work = true
+                var index = 0
+                copy[i] = i % 2 != 0 ? copy[i] : input[i]
+                while work {
+                    work = false
+                    for j in stride(from: copy.count - 1, to: i, by: -2) where copy[j] > 0 {
+                        if copy[j] <= copy[i] {
+                            copy[i] -= copy[j]
+                            work = copy[i] > 0
+                            res += (0..<copy[j]).reduce(0, { $0 + (len + index + $1) * (j / 2) })
+                            index += copy[j]
+                            copy[j] = 0
+                            break
+                        }
+                    }
+                }
+                copy[i] = i % 2 != 0 ? copy[i] : 0
+            }
+            len += thisLen
+        }
+        return res
     }
 }
