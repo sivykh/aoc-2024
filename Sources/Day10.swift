9e731ca190209e19c38047be1083b57090f05f8d
Sivykh Mikhail <m.sivykh@cian.ru>
2024-12-10 17:32:32 +0300

    Final code of the day 10

--- Sources/Day10.swift
+++ Sources/Day10.swift
@@ -4,20 +4,16 @@ import Collections
 struct Day10: AdventDay {
     var data: String
     
-    var entities: [[Int]] {
-        data.utf8CString.dropLast().map({ Int($0 - 48) }).split(separator: -38).map { Array($0) }
+    func part1() -> Any {
+        common().part1
     }
-    
+
     func part2() -> Any {
         common().part2
     }
     
-    func part1() -> Any {
-        common().part1
-    }
-    
     private func common() -> (part1: Int, part2: Int) {
-        let input = entities
+        let input = data.utf8CString.dropLast().map({ Int($0 - 48) }).split(separator: -38).map { Array($0) }
 
         func dfs(path: [(Int, Int)], row: Int, col: Int, current: inout Set<Cell>) -> Int {
             guard 0..<input.count ~= row, 0..<input[row].count ~= col, path.count == input[row][col] else {
